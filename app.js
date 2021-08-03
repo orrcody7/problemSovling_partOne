a20a1eb4d5b43e19679445b0b4d1a82f96ef17ab
unknown <orrcody7@gmail.com>
2021-08-03 10:28:45 -0600

    walked through code to understand each individual step and added notes to explain steps for future reference

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ for (let i = word.length -1; i >= 0; i--){
     console.log(word[i]);
 }
 
-// code works
+
 
 
 
@@ -23,3 +23,35 @@ let capitalizedWords = "this is fun";
     console.log(capitalizedWords[0].toUpperCase() + capitalizedWords.substring(1, 4) 
     + " " + capitalizedWords[5].toUpperCase() + capitalizedWords.substring(6, 7) 
     + " " + capitalizedWords[8].toUpperCase() + capitalizedWords.substring(9,11));
+
+// The numbers in the "()" after .substring relates to the index positions and
+// includes all indeces between those numbers.
+
+
+
+
+
+// 3) Compress a string of characters
+
+// An input of "aaabbbbbccccaacccbbbaaabbbaaa" would compress to
+// "3a5b4c2a3c3b3a3b3a"
+
+let letterString = 'fffeevvvv';
+function compressedString (str = '') {
+   let newString = '';
+   let count = 1;
+   for(let i = 0; i < str.length; i++){
+      let currentLetter = letterString[i]; //compares current element with next element in for loop
+      let nextLetter = letterString[i + 1];
+      if(currentLetter === nextLetter){
+         count++;
+      }else{
+         newString += currentLetter + String(count); 
+         //"String" = the text string "fffeevvvv" and (count) = the times each letter repeats
+         count = 1; // this resets the counter to 1 because no letter can have "0" as a value
+      };
+   }
+   return newString
+
+};
+console.log(compressedString(letterString));
